bddb473b0f268739d8736f647c9e84cbe74681cd
ParmS-Musale <musaleparm9541@gmail.com>
2025-05-13 18:38:52 +0530

    Improve documentation for ThemeProvider component

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,11 @@ import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
-
+// Export the ThemeProvider component from next-themes
+// and add the 'use client' directive to indicate that this component
+// should be rendered on the client side
+// This is necessary because the next-themes library relies on the browser's
+// localStorage API to persist the user's theme preference
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
